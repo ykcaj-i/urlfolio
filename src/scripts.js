552eaacaf25974fc3e0666cd8c2a5b330a6209da
ykcaj-i <jliuarc@gmail.com>
2024-11-08 15:40:01 -0500

    updated some css

--- src/scripts.js
+++ src/scripts.js
@@ -1,2 +1,31 @@
 
 
+
+
+
+class urlMaster {
+    constructor(url, date, category) {
+        this.url = url;
+        this.date = date;
+        this.category = category;
+    }
+}
+
+
+
+function validateURL(){ 
+    //validate the user's URL
+}
+
+function getDateOfURL(){ 
+    //once user's URL is validated, get the date and time of storage
+}
+
+function storeURL(){
+    //store URL in a database
+}
+
+function alphabeticizeURL(){
+
+}
+
