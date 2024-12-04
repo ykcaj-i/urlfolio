23de30a753991c1c1acdc3ec31b6be5058a13748
ykcaj-i <jliuarc@gmail.com>
2024-12-04 18:55:58 -0500

    added more css features

--- src/scripts.js
+++ src/scripts.js
@@ -10,15 +10,20 @@ let thisUrl;
         let Stringify = String(thisUrl);
         let isLink = true; 
         let Statement; //empty string
+        let storage = document.cookie;
+        const blankSpace = " ";
 
         if((Stringify.indexOf('https://') && Stringify.indexOf('.com') != isLink)){
-            Statement = "didn't pass";
+            Statement = "added a link";
             Stringify = "https://" + Stringify + ".com";
-            document.getElementById('favoritesUrl').textContent = `${Stringify} + ${Statement}`
+            document.getElementById('mainListUrl').innerHTML += `<div class = "wrap">${Stringify} + ${Statement}<br><br></div>` 
+            storage = `<div class = "wrap">${Stringify} + ${Statement}<br><br></div>`; 
         } else {
-            Statement = "pass";
-            document.getElementById('favoritesUrl').textContent = `${Stringify} + ${Statement}`
+            Statement = "didn't need to add link";
+            document.getElementById('mainListUrl').innerHTML += `<div class = "wrap">${Stringify} + ${Statement}<br><br></div>` 
+            storage = `<div class = "wrap">${Stringify} + ${Statement}<br><br></div>`; 
         }
+
         console.log(thisUrl);
         console.log(Stringify);
         
