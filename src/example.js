0468ffd5f66c7b9431927e9eb03a9ea89aef59aa
ykcaj-i <jliuarc@gmail.com>
2024-12-23 11:12:47 -0500

    added new function printArray and createArray

--- src/example.js
+++ src/example.js
@@ -8,13 +8,13 @@ document.getElementById('submit1').onclick = function(){
     const blankSpace = " ";
 
     if((Stringify.indexOf('https://') && Stringify.indexOf('.com') != isLink)){
-        console.log("added a link");
+            console.log("added a link");
         Stringify = "https://" + Stringify + ".com";
         Result =  `<div class = "wrap">${Stringify}<br><br></div>`;
         linkBeforeModification = Stringify;
         createHREF(Result, linkBeforeModification, Stringify);
     } else {
-        console.log("didn't add a link");
+            console.log("didn't add a link");
         Result =  `<div class = "wrap">${Stringify}<br><br></div>`;
         linkBeforeModification = Stringify;
         createHREF(Result, linkBeforeModification, Stringify);
@@ -22,20 +22,24 @@ document.getElementById('submit1').onclick = function(){
 }
 
 function createHREF(Result, linkBeforeModification, Stringify){ //check
-    console.log(Stringify + " = Stringify pass");
-    console.log(Result + " = Result pass");
-    console.log(linkBeforeModification + " = linkBeforeModification pass");
-    console.log("successful");
+        console.log(Stringify + " = Stringify pass");
+        console.log(Result + " = Result pass");
+        console.log(linkBeforeModification + " = linkBeforeModification pass");
+        console.log("successful");
+    let arrayLink = Stringify;
     let thisLink = Result;
     thisLink.href = linkBeforeModification;
     thisLink.target = '_blank';
     let tagHREF = `<a href = ${linkBeforeModification} target = "_blank">${linkBeforeModification}</a>`; //added an <a> href tag
     let fullyPaddedLink = `<div class = "wrap">${tagHREF}<br><br></div>`; //now the wrapped <a> href link has a div with a wrap class for styling effect
-    console.log(fullyPaddedLink + " = fullyPaddedLink pass");
+        console.log(fullyPaddedLink + " = fullyPaddedLink pass");
 
-    document.getElementById('mainListUrl').innerHTML += `<div class = "wrap">result=${Result} linkBeforeModification=<br> 
-    ${linkBeforeModification}<br><br> thisLink=${thisLink}fullyPaddedLink=${fullyPaddedLink}<br><br></div>`;
-
+    /*document.getElementById('mainListUrl').innerHTML += `<div class = "wrap">result=${Result} linkBeforeModification=<br> 
+    ${linkBeforeModification}<br><br> thisLink=${thisLink}fullyPaddedLink=${fullyPaddedLink}<br><br></div>`;*/
+        console.log(`this is the link we pass ${thisLink} and ${arrayLink}`);
+    createArray(arrayLink);
+    document.getElementById('mainListUrl').innerHTML += `<div class = "wrap">${fullyPaddedLink}</div>`;
+    
     /*
     let thisLink = Result;
     thisLink.href = 'linkBeforeModification';
@@ -47,16 +51,31 @@ function createHREF(Result, linkBeforeModification, Stringify){ //check
 }
 
 
-
-
-function createArray(thisLink){
-    let temp = thisLink;
+function createArray(arrayLink){
+    console.log("--function createArray---");
     const linkArray = [];
-    linkArray.push(thisLink);
+    let link = arrayLink;
+    let count = 0;
+    linkArray.push(link);
+    for(let i = 0; i < linkArray.length; i++){
+        console.log(`${linkArray} is in this array`);
+       
+    count++
+    }
+    
+    printArray(linkArray);
 }	
 
-function pushArray(){
+function printArray(linkArray){
+    let printThisArray = linkArray;
+    console.log("--function printArray---");
+    console.log(printThisArray);
+    document.getElementById('mainListArray').innerHTML += `<div class = "wrap">${linkArray}</div>`;   
+}
 
+
+function pushArray(){
+     
 }
 
 
