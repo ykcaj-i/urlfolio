a083c9d9c38415505d2fd0e200726f340b3046e3
ykcaj-i <jliuarc@gmail.com>
2025-01-28 17:46:52 -0500

    changed html

--- src/newexample.js
+++ src/newexample.js
@@ -1,35 +1,40 @@
+addEventListener("click", function(event){ //prevent page from auto refreshing
+    event.preventDefault()
+});
+
+
 
 //url submit
-document.getElementById('submit1').onclick = function(){
-    let urlFormValue = document.getElementById('url').value;
-    let url = String(urlFormValue);
+document.getElementById('submit1').onclick = function(){ // input form for the URL
+    let urlFormInputValue = document.getElementById('url').value;
+    let url = String(urlFormInputValue); // covert form input into a string
+    let originalUrl = url; // copy of URL to be stored as the href - eg: <a href = "originalUrl">our link</a>
+    let templateUrl; // this will be the data we will use to store the url that we will manipulate, both templateUrl and originalUrl will store the same string data type
+                     // but templateUrl will be used for manipulation while originalUrl wil be used purely just for the href
 
-
-    let originalUrl = url; //this is contains the url that we will use to make the link reference eg - href = "x.com"
-    let wrappedUrl; //this is the url that will be wrapped by the div - <div>x.com</div>
-    let newUrl;
     
-    console.log(`${originalUrl}`);
-    //check
-    if((url.indexOf('https://') == true) && (url.indexOf('.com') == true)){
-        //wrappedUrl = `<div class = "wrap"><br>${originalUrl}<br></div>`; //if this form has a proper link with https  and com, we wrap it and pass it off to the next function
-        wrapper(originalUrl);  //now that we have both a. originalUrl"https://google.com" and b. "`<div class = "wrap"><br>https://google.com<br></div>`" we send it to createHREF
-    } else {                                                    // else we we append the url with the proper format and then wrap afterwards
+    if((url.includes("https://")) && (url.includes(".com"))){ // if originalUrl has a valid url with proper formatting such as "https:// and .com" then we can pass on the value, and additionally store it into templateUrl for modification  
+        console.log("is a proper link");
+        templateUrl = originalUrl; 
+        htmlWrapper(templateUrl, originalUrl);  
+    } else {                                               
+        console.log("is not a proper link"); 
         originalUrl = "https://" + originalUrl + ".com";
-        //wrappedUrl = `<div class = "wrap"><br>${originalUrl}<br></div>`;
-        wrapper(originalUrl);
+        templateUrl = originalUrl;
+        htmlWrapper(templateUrl, originalUrl); 
     }
 }
 
-function wrapper(wrappedUrl, originalUrl){ //add an href to the wrapped url
-    //let giftWrappedUrl = wrappedUrl;
-    //giftWrappedUrl.href = originalUrl; 
-    //giftWrappedUrl.target = "_blank";
-    let giftWrappedUrl;
-    let hrefWrap = `<a href = ${originalUrl} target = "_blank"><br>${originalUrl}<br></a>`
-    let divWrap =  `<div class = "wrap">${hrefWrap}</div>`
+function htmlWrapper(originalUrl, templateUrl){ //add an href to the url
+    let wrappedUrl = `<a href = "${originalUrl}" target = "_blank"><br>"${templateUrl}"<br></a>`;
+    console.log("1. " +  `${wrappedUrl}`);
+    wrappedUrl = `<div class = "wrap">"${wrappedUrl}"</div>`;
+    console.log("2. " + `${wrappedUrl}`); //url has been wrapped with html properties making it a clickable link
 
+    //document.getElementById('urlList').innterHTML += `${wrappedUrl}`;
 }
+
+
 
 function createObject(){ //create an object with the div wrapped url and the copy of url
     
