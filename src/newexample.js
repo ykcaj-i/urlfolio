bbc68b12bb5d75543ffa3ae77fcb4a7113be547c
ykcaj-i <jliuarc@gmail.com>
2025-02-10 11:45:11 -0500

    added comment for js

--- src/newexample.js
+++ src/newexample.js
@@ -59,7 +59,21 @@ function openUrl(){ //experimental function to launch url
 
 
 
+/*
+now we need to create the function to store the category data type. once we get the string from category we have to create an array with it. we should use an object constructor
+and treat these categories as an oject, within the category is/are arrays, these arrays will store our url. 
+the url will first appear as its own separate entity, eventually we should add a button that asks if the user wants to add the url to either one of the category objects, joining its array
+every category box will so far, only be capable of storing a single array, the category id will be the name, and the array will inheret the name of the category box. 
+the array will be "cats" and the category container object will be called "Category-cats" so we'll need a name constructor for that too. 
+when the url are pushed onto an array with a function pushToArray, they will be stored into the specific corresponding array the users selected them into. then, multiple urls can..
+be added onto all active arrays and every single array will be displayed onto every category containers by default. 
+there will then be a final function called deployAll where the user can click a button to launch a for loop that will attempt to open every single website in the array/category.
+all the links will be stored into the array unwrapped, but it's cousin the wrappedUrl will be stored on the categorycontainer, this is so users can just click on the normal links..
+or if they want to automate the whole process, open all the links at the same time. 
 
+eventually we will add t
+
+*/
 
 
 
