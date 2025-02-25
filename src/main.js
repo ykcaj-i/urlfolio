a25ddadcf05bb557fa453e8e4a3187da22e306aa
ykcaj-i <jliuarc@gmail.com>
2025-02-24 22:49:16 -0500

    added is-url constructor

--- src/main.js
+++ src/main.js
@@ -8,8 +8,7 @@ function submitForm(event) {
     event.preventDefault();
  }
  form.addEventListener('submit', submitForm);
-
-
+ 
  var form = document.getElementById("categoryForm");//prevent site from refreshing on category form submission
  function submitForm(event) {
      event.preventDefault();
@@ -27,7 +26,6 @@ document.getElementById('submitUrl').onclick = function(){ //input form for the 
     let originalUrl = url; //copy of URL to be stored as the href - eg: <a href = "originalUrl">our link</a>
     let templateUrl; //this will be the data we will use to store the url that we will manipulate, both templateUrl and originalUrl will store the same string data type
                      //but templateUrl will be used for manipulation while originalUrl wil be used purely just for the href
-    
     if(((url.includes("https://")) || ((url.includes("www.")))) && (url.includes(".com"))){ //if originalUrl has a valid url with proper formatting such as "https:// and .com" then we can pass on the value, and additionally store it into templateUrl for modification  
         console.log("link ✓");
         templateUrl = originalUrl; 
